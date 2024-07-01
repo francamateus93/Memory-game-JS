775ad36566ee2b4e0d8b7b42c0f7f519c60a54c2
francamateus93 <francamateus@gmail.com>
2024-07-01 13:46:14 +0200

    tailwindok2

--- index.js
+++ index.js
@@ -7,3 +7,17 @@ let valuesUsed = []; // valores de los cards
 let currentMove = 0; // movimiento actual
 let currentAttempts = 0; // tentativas actuales
 
+// define una variavel para DIV
+let cardTemplate = '<div class="card"><div class="back"></div><div class="face"></div></div>';
+
+for (let i=0; i < totalCards; i++) { // iteracion para crear cada carta
+
+  let div = document.createElement('div'); // crea un 'div' en html
+
+  div.innerHTML = cardTemplate; // añade al 'div' los cards template
+
+  cards.push(div); // añade el 'div' al array cards
+
+  document.querySelector('#js-game').append(cards[i]); // añade al GAME el array com cartas creadas
+  // 
+}
